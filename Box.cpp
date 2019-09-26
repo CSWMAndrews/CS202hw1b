93b157c311493de2e7f76b483509573898a83412
Matt Andrews <wmandrews@alaska.edu>
2019-09-26 14:27:56 -0800

    swapped height and width to resolve mixup in incoming paramaters.

--- Box.cpp
+++ Box.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
+#include <sstream>      // for std::ostringstream
 #include "box.hpp"
+#include <string>       // for std::string
 
 using std::cout;
 using std::endl;
 
 
-Box::Box(void) :_Height(1)
+Box::Box(void) :_Height(1), _Width(1), _Filled(true)
 {
-	_Height = 1;
-	_Width = 1;
-	_Filled = true;
 	cout << "I made a default box and set it to " << _Height << " x " << _Width << " and " << _Filled << " !" << endl; //visible Constructor
 }
-Box::Box(int x, int y, bool f) : _Height(1)
+Box::Box(int x, int y, bool f)
 {
-	_Height = x;
-	_Width = y;
+	_Height = y;
+	_Width = x;
 	_Filled = f;
+	cout << "I made a box and set it to " << _Height << " x " << _Width << " and " << _Filled << " !" << endl; //visible Constructor
+}
+Box::Box(int x, int y)
+{
+	_Height = y;
+	_Width = x;
+	_Filled = true;
 	cout << "I made a box and set it to " << _Height << " x " << _Width << " and " << _Filled << " !" << endl; //visible Constructor
 }
 Box::~Box(void)
@@ -28,23 +34,100 @@ void Box::setWidth(int width)
 {
 	_Width = width;
 }
+void Box::setHeight(int height)
+{
+	_Height = height;
+}
+int Box::getWidth() const
+{
+	int width = _Width;
+	return width;
+}
+int Box::getHeight() const
+{
+	int height = _Height;
+	return height;
+}
 
-void Box::print(void) const
+std::string Box::type() const
+{
+	std::string type;
+	if (_Filled == true)
+		type = "Filled";
+	else
+		type = "Hollow";
+	return type;
+}
+
+//void Box::print(void) const
+//{
+//	int xX = 0;
+//	int yY = 0;
+//
+//	if (_Filled == true)
+//	{
+//		while (yY < _Height) 
+//		{
+//			xX = 0;
+//			while (xX < _Width) 
+//			{
+//				cout << "X";
+//				xX++;
+//			}
+//			cout << endl;
+//			yY++;
+//		}
+//	}
+//
+//	else
+//	{
+//		while (xX < _Width)
+//		{
+//			cout << "X";
+//			xX++;
+//		}
+//		cout << endl;
+//		xX = 0;
+//		while (yY < _Height - 2)
+//		{
+//			xX = 0;
+//
+//			cout << "X";
+//			while (xX < _Width - 2) 
+//			{
+//				cout << " ";
+//				xX++;
+//			}
+//
+//			cout << "X";
+//			cout << endl;
+//			yY++;
+//		}
+//		xX = 0;
+//		while (xX < _Width)
+//		{
+//			cout << "X";
+//			xX++;
+//		}
+//	}
+//}
+
+void Box::print(std::ostream & output) const
 {
 	int xX = 0;
 	int yY = 0;
 
 	if (_Filled == true)
 	{
-		while (yY < _Height) 
+		while (yY < _Height)
 		{
 			xX = 0;
-			while (xX < _Width) 
+			while (xX < _Width)
 			{
-				cout << "X";
+				output << "X";
 				xX++;
 			}
-			cout << endl;
+			output << endl;
 			yY++;
 		}
 	}
@@ -53,31 +136,32 @@ void Box::print(void) const
 	{
 		while (xX < _Width)
 		{
-			cout << "X";
+			output << "X";
 			xX++;
 		}
-		cout << endl;
+		output << endl;
 		xX = 0;
 		while (yY < _Height - 2)
 		{
 			xX = 0;
 
-			cout << "X";
-			while (xX < _Width - 2) 
+			output << "X";
+			while (xX < _Width - 2)
 			{
-				cout << " ";
+				output << " ";
 				xX++;
 			}
 
-			cout << "X";
-			cout << endl;
+			output << "X";
+			output << endl;
 			yY++;
 		}
 		xX = 0;
 		while (xX < _Width)
 		{
-			cout << "X";
+			output << "X";
 			xX++;
 		}
 	}
+
 }
--- box.hpp
+++ box.hpp
@@ -3,15 +3,22 @@
 #define BOX_BOX_H
 
 
+#include <sstream>      // for std::ostringstream
+#include <string>       // for std::string
+
+
 class Box {
 public:
 	void setWidth(int Width); //set the width of the box
 	int getWidth() const; //return the width of the box
 	void setHeight(int Height); //set the Height of the box
 	int getHeight() const; //return the Height of the box
-	void print() const; //print a box
+	//void print() const; //print a box
+	void print(std::ostream&) const; //print a box
+	std::string type() const;
 	Box();  // This is the constructor
 	Box(int, int, bool);  // This is the constructor
+	Box(int, int);  // This is the constructor
 	~Box();  // This is the destructor
 	//Box(const Line &original); // This is the copy constructor
 private:
